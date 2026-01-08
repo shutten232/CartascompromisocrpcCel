e162f974f53cab260d6a628f22f1dd1aa66fb5e0
gustavo castanares <115637674+shutten232@users.noreply.github.com>
2026-01-08 15:13:49 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "cartas-quirroga-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+  e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request)));
+});
